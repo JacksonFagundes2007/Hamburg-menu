ca3bdc88275d574061309fa260d547dac7004517
Jackson Fagundes <jacksonschirigatti.fag@gmail.com>
2021-02-24 09:01:36 -0300

    Update All

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,6 @@ function click() {
     let contentMenu = document.querySelector('#content-menu');
 
     btn.addEventListener('click', function() {
-
         btn.classList.toggle("active"),
         contentMenu.classList.toggle('remove'),
         contentMenu.classList.toggle('remove2')
